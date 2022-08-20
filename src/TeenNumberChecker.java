304f4fcf2d50ce07c32b21d5be73479c8359a157
MVBALA <43174220+MVBALA@users.noreply.github.com>
2022-08-20 20:41:30 +0530

    Update TeenNumberChecker.java

--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class TeenNumberChecker {
+public class TeenNumberChecker {
+    public static boolean hasTeen(int a, int b, int c) {
+        if (isTeen(a) || isTeen(b) || isTeen(c) )
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean isTeen(int a) {
+        if (a >= 13 && a <= 19)
+            return true;
+        else
+            return false;
+    }
 }
